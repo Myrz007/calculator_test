3cc1d4794dc06b5225bd8feb510f09b42e154d6c
Myrz007 <122377787+Myrz007@users.noreply.github.com>
2023-04-12 16:36:22 +0200

    Add modulo function

--- js/main.js
+++ js/main.js
@@ -24,6 +24,12 @@ function divide(num1, num2) {
     return Math.round(num1 / num2 * 1000) / 1000;
 }
 
+function modulo(num1, num2) {
+    if (num2 === 0) return 'To divide by zero is not defined';
+
+    return num1 % num2;
+}
+
 function operate(operator, num1, num2) {
     const operations = {
         '+': add,
